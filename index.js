f9a1806487285e30a7ff6700f3e9c4444d39154b
Jonathan Samson <jonathansamsonvcu@gmail.com>
2020-08-23 15:09:10 -0400

    add flags: -d <date> -o <offset>, filename arg

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const { program } = require('commander');
 const { spawn, spawnSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const os = require('os')
+const os = require('os');
+const moment = require('moment');
+
+const USER_SETTINGS_PATH = path.join(os.homedir(), '.notes_config');
+const DATE_FLAG = '<date>';
+const FILENAME_FLAG = '<filename>';
 
 class Settings {
     static defaultSettings = {
         notesPath: path.join(os.homedir(), 'Notes'),
+        extension: 'txt',
+        template: `# <date> (<filename>)`,
+        templateFile: null,
     }
 
     constructor(options) {
@@ -17,15 +25,31 @@ class Settings {
         };
 
         this.notesPath = s.notesPath;
+        this.extension = s.extension;
+        this.template = s.template;
+        this.templateFile = s.templateFile;
     }
+}
+
+function fromTemplate(template, { date, filename }) {
+    template = template.replace(DATE_FLAG, date);
+    template = template.replace(FILENAME_FLAG, filename);
+    return template;
 }
 
 /**
  * Get the date string used for folder naming.
- * @param {Date} date 
+ * @param {moment.Moment} date 
  */
 function getDateString(date) {
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    return date.toISOString().slice(0, 10);
+}
+
+function correctExtension(filename, extension) {
+    const parts = filename.split('.');
+    if (parts.length < 2)
+        return `${filename}.${extension}`;
+    return filename;
 }
 
 /**
@@ -36,38 +60,74 @@ function openFileInVim(path) {
     spawn('vim', [path], { stdio: 'inherit' });
 }
 
-function mkdirSafe(path) {
-    spawnSync('mkdir', ['-p', path]);
+function loadUserSettings() {
+    if (!fs.existsSync(USER_SETTINGS_PATH)) {
+        fs.writeFileSync(
+            USER_SETTINGS_PATH,
+            JSON.stringify(Settings.defaultSettings, null, 2) + '\n'
+        );
+    }
+
+    return JSON.parse(
+        String(fs.readFileSync(USER_SETTINGS_PATH))
+    );
 }
 
-function main() {
-    const settings = new Settings();
 
-    // Use the current date as the folder name
-    const dateString = getDateString(new Date());
+function main(filename = 'default.txt', {
+    date,
+    offset,
+}) { 
+    const settings = new Settings(loadUserSettings());
 
-    mkdirSafe(path.join(
-        settings.notesPath,
-        dateString
-    ));
+    filename = correctExtension(filename, settings.extension);
 
-    const filename = 'default.txt';
+    console.log(offset);
+    
+    offset = Number(offset);
+    if (Number.isNaN(offset)) offset = 0;
 
-    // we just write to defaut.txt for now
+    // use the current date as the folder name
+    const dateString = getDateString(moment(date).add(offset, 'day'));
+
+    console.log(dateString, date, offset);
+
+    // create directories
+    fs.mkdirSync(
+        path.join(settings.notesPath, dateString),
+        { recursive: true }     
+    );
+
     const file = path.join(
         settings.notesPath,
         dateString,
         filename
     );
 
-    // create notes file with auto-generated first line
+    // create notes file
     if (!fs.existsSync(file)) {
-        const data = `#${dateString} - ${filename}`;
+        const data = fromTemplate(settings.template, {
+            date: dateString,
+            filename,
+            filepath: file,
+        });
         fs.writeFileSync(file, data);
     }
 
-    // open file in vim
-    spawn('vim', [file], { stdio: 'inherit' });
+    openFileInVim(file)
 }
 
-main();
+program
+    .version('0.1.0')
+    .description('A simple cli for taking daily notes.')
+    .arguments('[filename]')
+    .option("-d --date <date>", "provide a date", getDateString(moment()))
+    .option("-o --offset <offset>", "number of days ago (-) or in future (+)", 0)
+    .action((filename, cmdObj) => {
+        main(filename, {
+            date: cmdObj.date,
+            offset: cmdObj.offset,
+        });
+    });
+
+program.parse(process.argv);
